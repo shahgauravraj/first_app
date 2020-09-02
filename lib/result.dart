ed193d976ea9d36497429619a3b02f9efafc856c
Gaurav <shahgauravraj@gmail.com>
2020-09-02 14:38:36 +0545

    displays results and a dedicated restart button

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int finalScore;
+  final Function resetHandler;
+
+  Result(this.finalScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resultText =
+        'Congratulations!\nYou Scored ' + finalScore.toString() + ' Points.';
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text(
+              'Retake Quiz',
+            ),
+            textColor: Colors.blue,
+            onPressed: resetHandler,
+          )
+        ],
+      ),
     );
   }
 }
